93e4617c5e8413734679cde998c3bb378538ec34
Christine Coulter <christinetcoulter@gmail.com>
2013-07-03 16:49:06 -0400

    Write post / block to include sunlight command for zip code.

--- main.rb
+++ main.rb
@@ -14,6 +14,15 @@ Sunlight::Base.api_key = '035fbbb591aa4fdba299e64c0d9e867c'
 #   host: 'localhost'
 # }
 
+# This should show index
+# Search box and list FAVORITE LEGISLATORS
 get '/' do
  erb :index
 end
+
+post '/' do
+  zip = params[:zip]
+  legislator = Sunlight::Legislator.all_in_zipcode(zip)
+  redirect to('/search_results')
+end
+
