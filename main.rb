730022e4d845f8cb9f5b4c7feaeb9485144284f6
nicklombardi <nicklombardi@gmail.com>
2013-07-03 18:24:21 -0400

    created results page

--- main.rb
+++ main.rb
@@ -17,8 +17,23 @@ set :database, {
 
 Sunlight::Base.api_key = 'e9f3c1270bd24c6980e3408a7179ba12'
 
+# binding.pry
 
-get '/'  do
+get '/' do
   erb :index
+end
+
+get '/results' do
+  @zip = params[:zip]
+  @legislators = Sunlight::Legislator.all_in_zipcode(@zip)
+  erb :results
+end
+
+post '/fav' do
+  # get unique id from results.erb
+  # new api call using unique id
+  # send info to database
+  # redirect to index
+  "#{params}"
 end
 
