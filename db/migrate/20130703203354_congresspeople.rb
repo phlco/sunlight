bbf972a316d34dca130859c19ed1a2098a8c651a
mvanger <mvanger@nyu.edu>
2013-07-03 16:37:23 -0400

    created congresspeople table

---
+++ db/migrate/20130703203354_congresspeople.rb
@@ -0,0 +1,17 @@
+class Congresspeople < ActiveRecord::Migration
+  def up
+    create_table :congresspeople do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :party
+      t.string :phone
+      t.string :state
+      t.string :twitter_id
+      t.boolean :in_office, default = true
+    end
+  end
+
+  def down
+    drop_table :congresspeople
+  end
+end
