5e557f3e1a9c7d062cd29ff0da07e196b5a54ea9
mvanger <mvanger@nyu.edu>
2013-07-03 17:16:55 -0400

    set up api maybe?

--- main.rb
+++ main.rb
@@ -4,6 +4,13 @@ require 'pg'
 require 'pry'
 require 'sinatra/activerecord'
 require 'sunlight'
+require 'dotenv'
+
+
+configure do
+  Dotenv.load
+  Sunlight::Base.api_key = 'SUNLIGHT_API_KEY'
+end
 
 set :database, {
   adapter: 'postgresql',
@@ -11,6 +18,18 @@ set :database, {
   host: 'localhost'
 }
 
+class Congressperson < ActiveRecord::Base
+end
+
 get '/' do
   erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/' do
+  zip_code = params[:zip_code]
+  @congresspeople = Sunlight::Legislator.all_in_zipcode(zip_code)
 end
