e4f48a3b8b66a25fe5840d6257ab1dbfa950d3d9
Wesley Carr <surfswell17@gmail.com>
2013-07-03 21:26:12 -0400

    Finally pulling info from sunlight after assigning the (zipcode) to an instance variable

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'sunlight'
+
 Sunlight::Base.api_key = '035fbbb591aa4fdba299e64c0d9e867c'
 
 set :database, {
@@ -14,17 +15,19 @@ set :database, {
 }
 
 class Politician < ActiveRecord::Base
-
 end
 
 get '/' do
   erb :index
 end
 
-get '/politicians' do
-  zipcode = params[:zipcode]
-  #Defining instance variable with Sunlight app
-  @politicians = Sunlight::Legislator.all_in_zipcode(zipcode)
-  erb :politicians
+post '/' do
+  redirect to("/#{params[:zipcode]}")
 end
-binding.pry
+
+get '/:zipcode' do
+  @zipcode = params[:zipcode]
+  #Defining instance variable with Sunlight app
+  @politicians = Sunlight::Legislator.all_in_zipcode(@zipcode)
+  erb :politicians_in_zipcode
+end
