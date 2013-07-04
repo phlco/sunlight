53a0b0361a9f7cb11b294cc80e3b7dd72e4ffb21
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-03 21:11:39 -0400

    Added blue and red and purple to represent diff party

--- main.rb
+++ main.rb
@@ -15,6 +15,9 @@ set :database, {
   host: 'localhost'
 }
 
+class Legislator < ActiveRecord::Base
+end
+
 get '/' do
   erb :index
 end
@@ -27,6 +30,20 @@ end
 get '/:zipcode' do
   code = params[:zipcode]
   @results = Sunlight::Legislator.all_in_zipcode(code)
-
   erb :results
+end
+
+post '/legislator/:phone' do
+  phone = params[:phone]
+  fav_legislator = Sunlight::Legislator.all_where(:phone => phone)
+  firstname = fav_legislator[0].firstname
+  lastname = fav_legislator[0].lastname
+  party = fav_legislator[0].party
+  votesmart_id = fav_legislator[0].votesmart_id
+  state = fav_legislator[0].state
+  twitter_id = fav_legislator[0].twitter_id
+  in_office = fav_legislator[0].in_office
+
+  leg = Legislator.create(firstname: firstname, lastname: lastname, party: party, phone: phone, state: state, twitter_id: twitter_id, in_office: in_office, votesmart_id: votesmart_id)
+  redirect to("/")
 end
