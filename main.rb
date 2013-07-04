18d57f2261fcd121d1dd55095d5de72f8078beef
nicklombardi <nicklombardi@gmail.com>
2013-07-03 23:27:35 -0400

    first attempt at database submission

--- main.rb
+++ main.rb
@@ -14,12 +14,15 @@ set :database, {
   host: 'localhost'
 }
 
+class Favorites < ActiveRecord::Base
+end
 
 Sunlight::Base.api_key = 'e9f3c1270bd24c6980e3408a7179ba12'
 
 # binding.pry
 
 get '/' do
+  @favorites = Favorites.all
 
   # access database to display favorites
   # add party colors
@@ -33,14 +36,22 @@ get '/results' do
 end
 
 post '/fav' do
+  favorite = Favorites.find(params[:id])
   @bioguide_id = params[:bioguide_id]
-  @favorite = Sunlight::Legislator.all_where(:bioguide_id => "@bioguide_id")
-  firstname - @favorite.firstname
+  favorite = Sunlight::Legislator.all_where(:bioguide_id => "@bioguide_id")
+  favorite[10] = params[:firstname]
+  favorite[14] = params[:lastname]
+  favorite[18] = params[:party]
+  favorite[19] = params[:phone]
+  favorite[21] = params[:state]
+  favorite[22] = params[:twitter_id]
+  favorite[13] = params[:in_office]
+  favorite[24] = params[:votesmart_id]
   # get unique id from results.erb
   # new api call using unique id
   # send info to database
-  "#{params}"
-  # redirect to "/"
+  # "#{params}"
+  redirect to "/"
 end
 
 
