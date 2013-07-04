5542029b1c9251990602e74b225cdcae2542d053
nicklombardi <nicklombardi@gmail.com>
2013-07-03 20:47:34 -0400

    db migrated

--- main.rb
+++ main.rb
@@ -20,6 +20,7 @@ Sunlight::Base.api_key = 'e9f3c1270bd24c6980e3408a7179ba12'
 # binding.pry
 
 get '/' do
+
   # access database to display favorites
   # add party colors
   erb :index
@@ -32,10 +33,17 @@ get '/results' do
 end
 
 post '/fav' do
+  @bioguide_id = params[:bioguide_id]
+  @favorite = Sunlight::Legislator.all_where(:bioguide_id => "@bioguide_id")
+  firstname - @favorite.firstname
   # get unique id from results.erb
   # new api call using unique id
   # send info to database
-  # redirect to index
   "#{params}"
+  # redirect to "/"
 end
+
+
+
+
 
