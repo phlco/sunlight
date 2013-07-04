1abb742403c93f88df710fff1ee5349709e2c20d
eleyna <eleyna@gmail.com>
2013-07-04 17:06:41 -0400

    done, just a bit of clean-up to do

--- main.rb
+++ main.rb
@@ -5,6 +5,8 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
+require 'yaml'
+
 Sunlight::Base.api_key = '035fbbb591aa4fdba299e64c0d9e867c'
 
 set :database, {
@@ -17,14 +19,19 @@ class Favorite_Legislator < ActiveRecord::Base
 end
 
 get '/' do
-  @favorites = []
-  #= Favorite_Legislator.all
+  @favorites = Favorite_Legislator.all
   erb :index
 end
 
 get '/legislators' do
   @members_of_congress = Sunlight::Legislator.all_in_zipcode(params[:zip])
   erb :legislators
+end
+
+post '/favorited' do
+  fave = YAML.load(params[:favorited])
+  Favorite_Legislator.create(:firstname => fave.firstname, :lastname => fave.lastname, :party => fave.party, :phone => fave.phone, :state => fave.state, :twitter_id => fave.twitter_id, :in_office => fave.in_office, :votesmart_id => fave.votesmart_id)
+  redirect to '/'
 end
 
 
