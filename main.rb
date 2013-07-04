a7bf920606d86367438c5c57ed584fe4979a5bad
Zachariah Reitano <zreitano@gmail.com>
2013-07-03 23:24:29 -0400

    Full Functionality

--- main.rb
+++ main.rb
@@ -19,6 +19,7 @@ class Legislator < ActiveRecord::Base
 end
 
 get '/' do
+  @favorite_legislators = Legislator.all
   erb :index
 end
 
@@ -26,6 +27,13 @@ get '/legislators' do
   zipcode = params[:zipcode]
   @legislators = Sunlight::Legislator.all_in_zipcode(zipcode)
   erb :legislators
+end
+
+post '/new_favorite_legislator' do
+  votesmart_id = params[:votesmart_id].to_i
+  legislator = Sunlight::Legislator.all_where(:votesmart_id => votesmart_id).first
+  this_legislator = Legislator.create(first: legislator.firstname, last: legislator.lastname, phone: legislator.phone, state: legislator.state, twitter_id: legislator.twitter_id, in_office: legislator.in_office, votesmart_id: legislator.votesmart_id)
+  redirect to '/'
 end
 
 
