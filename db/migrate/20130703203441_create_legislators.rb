bfb3a89e54b7bfd6f7e7c4f39f86193f9d10a4c9
Beryl <beryl.schragger@gmail.com>
2013-07-03 20:54:36 -0400

    Add database, ability to add favorites to database, and main page favorites list.

--- db/migrate/20130703203441_create_legislators.rb
+++ db/migrate/20130703203441_create_legislators.rb
@@ -1,18 +1,19 @@
 class CreateLegislators < ActiveRecord::Migration
   def up
     create_table :legislators do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :firstname
+      t.string :lastname
       t.string :party
       t.string :phone
       t.string :state
       t.string :twitter_id
       t.string :in_office
-      t.string :votesmart_id
+      t.integer :votesmart_id
       t.timestamps
     end
   end
 
   def down
+    drop_table :legislators
   end
 end
--- main.rb
+++ main.rb
@@ -7,11 +7,18 @@ require 'rubygems'
 require 'sunlight'
 Sunlight::Base.api_key = '50479a20636a4d47b1fa69560e2b18b9'
 
+set :database, {
+              :adapter => 'postgresql',
+              :database => 'congress',
+              :host => 'localhost'
+}
+
 class Legislator < ActiveRecord::Base
 end
 
 get '/' do
   # list of favored legislators
+  @favorites = Legislator.all
   erb :index
 end
 
@@ -21,9 +28,14 @@ post '/' do
 end
 
 get '/:zip' do
-  @congresspeople = Sunlight::Legislator.all_in_zipcode(params[:zip])
+  @zip = params[:zip]
+  @congresspeople = Sunlight::Legislator.all_in_zipcode(@zip)
   erb :zip
 end
 
+post '/add_to_favorites' do
+  cp = Legislator.create(params)
+  redirect to "/"
+end
 # cp = Sunlight::Legislator.all_in_zipcode(19803)
 #   binding.pry
