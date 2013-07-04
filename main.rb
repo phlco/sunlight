43930590117cd09ef8c02552243845e6fa3695b4
Wesley Carr <surfswell17@gmail.com>
2013-07-03 22:32:40 -0400

    Minor format changes

--- main.rb
+++ main.rb
@@ -20,11 +20,13 @@ end
 get '/' do
   erb :index
 end
-
+#Have to transfer the zipcode onto the new page to display politicians
+#of that certain area
 post '/' do
   redirect to("/#{params[:zipcode]}")
 end
-
+#NOT '/politicians' - cannot pass zipcode that way.
+# assigned the url to the zipcode parameters.
 get '/:zipcode' do
   @zipcode = params[:zipcode]
   #Defining instance variable with Sunlight app
