20aad4371072401ae90c9621c43664d58d4184ff
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-06 23:04:33 -0400

    Creaged the db table FavLegislators

--- main.rb
+++ main.rb
@@ -43,37 +43,16 @@ get '/result' do
 end
 
 post '/savefav' do
+
+  # t.string :firstname
+  # t.string :lastname
+  # t.string :party
+  # t.string :phone
+  # t.string :twitter_id
+  # t.boolean :in_office
+  # t.string :votesmart_id
+
+
+
   redirect to("/")
 end
-
-
-
-
-# @bioguide_id="N000002",
-# @birthdate="10947-06-13 00:00:00 -0400",
-# @congress_office="2110 Rayburn House Office Building",
-# @congresspedia_url="http://www.opencongress.org/wiki/Jerrold_Nadler",
-# @crp_id="N00000939",
-# @district="10",
-# @email="",
-# @facebook_id="CongressmanNadler",
-# @fax="202-225-6923",
-# @fec_id="H2NY17071",
-# @firstname="Jerrold",
-# @gender="M",
-# @govtrack_id="400289",
-# @in_office=true,
-# @lastname="Nadler",
-# @middlename="L.",
-# @name_suffix="",
-# @nickname="",
-# @party="D",
-# @phone="202-225-5635",
-# @senate_class="",
-# @state="NY",
-# @title="Rep",
-# @twitter_id="RepJerryNadler",
-# @votesmart_id="26980",
-# @webform="http://www.house.gov/nadler/emailform.shtml",
-# @website="http://www.house.gov/nadler",
-# @youtube_url="http://youtube.com/congressmannadler"
